2801ca57c9c94f1cf0fd5f1e12685e1c36b51856
Mike Rhodes <mike.rhodes@gmail.com>
2012-10-13 16:46:48 +0100

    Test multiple files in the delta.

--- src/collective/dropboxfolder/tests/test_sync.py
+++ src/collective/dropboxfolder/tests/test_sync.py
@@ -85,6 +85,136 @@ class TestDropboxSync(unittest.TestCase):
         self.assertEqual(77, metadata['bytes'])
         self.assertEqual('362e2029684fe', metadata['rev'])
 
+    def test_odd_filename(self):
+        container = self.portal
+
+        metadata = json.loads("""
+            {
+               "bytes": 77,
+               "icon": "page_white_text",
+               "is_dir": false,
+               "mime_type": "text/plain",
+               "modified": "Wed, 20 Jul 2011 22:04:50 +0000",
+               "path": "/A long and mysterious filename",
+               "rev": "362e2029684fe",
+               "revision": 221922,
+               "root": "dropbox",
+               "size": "77 bytes",
+               "thumb_exists": false
+           }
+        """)
+
+        sync_data = {
+                "entries": [
+                    ["/a-long-and-mysterious-filename", metadata],
+                    ],
+                "reset": False,
+                "cursor": "1",
+                "has_more": False,
+                }
+
+        self.sync.delta_response.append(sync_data)
+
+        container = self.portal
+        container_fti = container.getTypeInfo()
+    
+        if container_fti is not None and not container_fti.allowType(DROPBOX_FOLDER_TYPE):
+            raise ValueError("Disallowed subobject type: %s" % (DROPBOX_FOLDER_TYPE,))
+        
+        ob = createContentInContainer(container,
+                                      DROPBOX_FOLDER_TYPE,
+                                      checkConstraints=False,
+                                      id="dropboxfolder",
+                                      )
+
+        processor = IDropboxSyncProcessor(ob)
+        processor.sync()
+
+        self.assertEqual(1, len(ob))
+        self.assertIsNotNone(ob.get("a-long-and-mysterious-filename", None))
+
+        metadata = IDropboxMetadata(ob['a-long-and-mysterious-filename']).get()
+        self.assertIsNotNone(metadata)
+        self.assertEqual(77, metadata['bytes'])
+        self.assertEqual('362e2029684fe', metadata['rev'])
+
+    
+    def test_multiple_files(self):
+        container = self.portal
+
+        metadata1 = json.loads("""
+            {
+               "bytes": 77,
+               "icon": "page_white_text",
+               "is_dir": false,
+               "mime_type": "text/plain",
+               "modified": "Wed, 20 Jul 2011 22:04:50 +0000",
+               "path": "/magnum-opus.txt",
+               "rev": "362e2029684fe",
+               "revision": 221922,
+               "root": "dropbox",
+               "size": "77 bytes",
+               "thumb_exists": false
+           }
+        """)
+
+        metadata2 = json.loads("""
+            {
+               "bytes": 1230,
+               "icon": "page_white_text",
+               "is_dir": false,
+               "mime_type": "text/plain",
+               "modified": "Wed, 20 Jul 2011 22:04:50 +0000",
+               "path": "/Somewhere_over_the_rainbow.txt",
+               "rev": "362e",
+               "revision": 221922,
+               "root": "dropbox",
+               "size": "77 bytes",
+               "thumb_exists": false
+           }
+        """)
+
+        sync_data = {
+                "entries": [
+                    [metadata1['path'], metadata1],
+                    [metadata2['path'], metadata2],
+                    ],
+                "reset": False,
+                "cursor": "1",
+                "has_more": False,
+                }
+
+        self.sync.delta_response.append(sync_data)
+
+        container = self.portal
+        container_fti = container.getTypeInfo()
+    
+        if container_fti is not None and not container_fti.allowType(DROPBOX_FOLDER_TYPE):
+            raise ValueError("Disallowed subobject type: %s" % (DROPBOX_FOLDER_TYPE,))
+        
+        ob = createContentInContainer(container,
+                                      DROPBOX_FOLDER_TYPE,
+                                      checkConstraints=False,
+                                      id="dropboxfolder",
+                                      )
+
+        processor = IDropboxSyncProcessor(ob)
+        processor.sync()
+
+        self.assertEqual(2, len(ob))
+
+        self.assertIsNotNone(ob.get("magnum-opus.txt", None))
+        metadata = IDropboxMetadata(ob['magnum-opus.txt']).get()
+        self.assertIsNotNone(metadata)
+        self.assertEqual(77, metadata['bytes'])
+        self.assertEqual('362e2029684fe', metadata['rev'])
+
+        self.assertIsNotNone(ob.get("somewhere_over_the_rainbow.txt", None))
+        metadata = IDropboxMetadata(ob['somewhere_over_the_rainbow.txt']).get()
+        self.assertIsNotNone(metadata)
+        self.assertEqual(1230, metadata['bytes'])
+        self.assertEqual('362e', metadata['rev'])
+
 
 
 
