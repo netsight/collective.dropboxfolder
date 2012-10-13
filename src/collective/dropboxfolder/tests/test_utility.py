7cc08823fcc80f14e9a92f6320c9098cccf60d26
Matthew Sital-Singh <matthew.sitalsingh@gmail.com>
2012-10-13 17:29:08 +0100

    A few test for the auth utility

--- src/collective/dropboxfolder/tests/test_utility.py
+++ src/collective/dropboxfolder/tests/test_utility.py
@@ -4,6 +4,7 @@ from zope.component import getUtility
 from collective.dropboxfolder.testing import\
     COLLECTIVE_DROPBOXFOLDER_INTEGRATION
 from collective.dropboxfolder.interfaces import IDropboxAuth
+from collective.dropboxfolder.utilities import DropboxAuthException
 
 
 class TestDropboxAuth(unittest.TestCase):
@@ -14,10 +15,28 @@ class TestDropboxAuth(unittest.TestCase):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
 
-    def test_utility_registered(self):
+    def test_app_secret(self):
+        """ get and set the app secret """
+        test_secret = 'x1c2v3b4'
         auth = getUtility(IDropboxAuth)
-        linked = auth.is_linked()
-        self.assertTrue(isinstance(linked, bool),
-                        'Expected boolean value')
-        self.assertFalse(linked,
-                         'Auth should not be linked by default')
+        self.assertFalse(auth.get_app_secret())
+        auth.set_app_secret(test_secret)
+        self.assertEquals(auth.get_app_secret(), test_secret)
+
+    def test_is_linked(self):
+        """ not linked by default """
+        auth = getUtility(IDropboxAuth)
+        self.assertFalse(auth.is_linked())
+
+    def test_build_authorize_url(self):
+        """ test that custom exception is raised """
+        auth = getUtility(IDropboxAuth)
+        self.assertRaises(DropboxAuthException,
+                          auth.build_authorize_url)
+
+    def test_obtain_access_token(self):
+        """ test that we cant do this without a valid token """
+        auth = getUtility(IDropboxAuth)
+        self.assertFalse(
+            auth.obtain_access_token('token', 'code')
+        )
