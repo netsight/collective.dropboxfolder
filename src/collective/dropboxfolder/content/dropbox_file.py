8e59fde3dd7379838b462ab3fd440d606208ec65
Matthew Sital-Singh <matthew.sitalsingh@gmail.com>
2012-10-14 12:13:37 +0100

    Corrected file field name

--- src/collective/dropboxfolder/content/dropbox_file.py
+++ src/collective/dropboxfolder/content/dropbox_file.py
@@ -4,24 +4,24 @@ from zope import schema
 from plone.directives import form, dexterity
 
 from plone.app.textfield import RichText
-from plone.namedfile.field import NamedFile
+from plone.namedfile.field import NamedBlobFile
 
 from collective.dropboxfolder import _
 
 class IDropboxFile(form.Schema):
     """A folder which syncs its contents with a given Dropbox account folder.
     """
-    
+
     title = schema.TextLine(
             title=_(u"Name"),
         )
-    
+
     description = schema.Text(
             title=_(u"A short summary"),
         )
 
-    fiile = NamedFile(
+    file_data = NamedBlobFile(
             title=_(u"File"),
         )
 
-    
+
