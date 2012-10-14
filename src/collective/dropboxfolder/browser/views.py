5b65c266d17c31b95f08c0353132758f05b75462
Matthew Sital-Singh <matthew.sitalsingh@gmail.com>
2012-10-14 12:14:01 +0100

    Added status message and redirect to browser view

--- src/collective/dropboxfolder/browser/views.py
+++ src/collective/dropboxfolder/browser/views.py
@@ -1,4 +1,5 @@
 from Products.Five.browser import BrowserView
+from Products.statusmessages.interfaces import IStatusMessage
 
 from collective.dropboxfolder.interfaces import IDropboxSyncProcessor
 
@@ -8,3 +9,6 @@ class SyncFolder(BrowserView):
     def sync_now(self):
         syncer = IDropboxSyncProcessor(self.context)
         syncer.sync()
+
+        IStatusMessage(self.request).addStatusMessage("Dropbox sync complete.")
+        return self.request.response.redirect(self.context.absolute_url())
